505b55a75dfc685a31242a0082352b7a3c3322c3
NYSAJ <pugpug11@gmail.com>
2026-03-17 17:56:39 +0900

    補足をコメント記入

--- counter.js
+++ counter.js
@@ -16,3 +16,17 @@
       [i].addEventListener("click", (e) => clickHandler(e));
   }
 })();
+
+// const $buttons = document.getElementsByClassName("js-button");
+// console.log($buttons);
+
+// const clickHandler = (e) => console.log(e);
+
+// for (let i = 0; i < document.getElementsByClassName("js-button").length; i++) {
+//   document.getElementsByClassName("js-button")[i].addEventListener("click", (e) => clickHandler(e));
+// }
+
+// const clickHandler = (e) => console.log(e.currentTarget);
+// for (let i = 0; i < document.getElementsByClassName("js-button").length; i++) {
+//   document.getElementsByClassName("js-button")[i].addEventListener("click", (e) => clickHandler(e));
+// }

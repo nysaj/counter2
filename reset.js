671ce23fa0912edc6cefc68e47faf75e8b6c1406
NYSAJ <pugpug11@gmail.com>
2026-03-20 09:47:55 +0900

    リセット時のコメント追加

--- reset.js
+++ reset.js
@@ -1,8 +1,10 @@
 (() => {
   const $counter = document.getElementById("js-counter");
+  const $result = document.getElementById("result");
 
   const clickHandler = () => {
     $counter.textContent = 0;
+    $result.textContent = "0です";
   };
 
   document.getElementById("js-reset-button").addEventListener("click", clickHandler);
